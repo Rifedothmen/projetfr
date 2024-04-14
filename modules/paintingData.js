51c8718a642384a43e18c2847c2bd9cb178a4f4b
oussamaassal <ahmedoussama.assal@esprit.tn>
2024-04-15 00:55:55 +0100

    added images

--- modules/paintingData.js
+++ modules/paintingData.js
@@ -1,67 +1,214 @@
 export const paintingData = [
   // Front Wall
-  ...Array.from({ length: 4 }, (_, i) => ({
-    // Array.from creates an array from an array-like object. The first parameter is the array-like object. The second parameter is a map function that is called for each element in the array-like object. The map function takes two parameters: the element and the index. The map function returns the value that will be added to the new array. In this case, we are returning an object with the painting data. `_` is a placeholder for the element. We don't need it because we are not using the element. `i` is the index. We use it to set the painting number.
-    imgSrc: `artworks/${i + 1}.jpg`, // `i + 1` is the painting number. We add 1 to the index because the index starts at 0 but we want the painting numbers to start at 1.
-    width: 5, // width of the painting
-    height: 3, // height of the painting
-    position: { x: -15 + 10 * i, y: 2, z: -19.5 }, // position of the painting
-    rotationY: 0, // rotation of the painting
-    info: { // info about the painting
-      title: `Van Gogh ${i + 1}`, 
-      artist: 'Vincent van Gogh',
-      description: `This is one of the masterpieces by Vincent van Gogh, showcasing his unique style and emotional honesty. Artwork ${
-        i + 1
-      } perfectly encapsulates his love for the beauty of everyday life.`,
-      year: `Year ${i + 1}`,
-    },
-  })),
-  // Back Wall
-  ...Array.from({ length: 4 }, (_, i) => ({
-    imgSrc: `artworks/${i + 5}.jpg`,
+  {
+    imgSrc: "artworks/1.jpg",
     width: 5,
     height: 3,
-    position: { x: -15 + 10 * i, y: 2, z: 19.5 },
+    position: { x: -15, y: 2, z: -19.5 },
+    rotationY: 0,
+    info: {
+      title: "Évolution de la Communication",
+      artist: "Vincent van Gogh",
+      description: `La communication humaine a connu une évolution remarquable depuis les premiers signes et gestes jusqu'aux technologies numériques sophistiquées d'aujourd'hui.`,
+      year: "Year 1",
+    },
+  },
+  {
+    imgSrc: "artworks/2.jpg",
+    width: 5,
+    height: 3,
+    position: { x: -5, y: 2, z: -19.5 },
+    rotationY: 0,
+    info: {
+      title: "Communication Non Verbale",
+      artist: "Vincent van Gogh",
+      description: `Les hiéroglyphes égyptiens ont introduit une forme écrite de communication, permettant la transmission d'informations sur des parchemins et des surfaces de pierre.`,
+      year: "Year 2",
+    },
+  },
+  {
+    imgSrc: "artworks/3.jpg",
+    width: 5,
+    height: 3,
+    position: { x: 5, y: 2, z: -19.5 },
+    rotationY: 0,
+    info: {
+      title: "Émergence du Langage Parlé",
+      artist: "Vincent van Gogh",
+      description: `Avec l'émergence du langage parlé, les sociétés humaines ont pu échanger des idées de manière plus complexe et nuancée, favorisant le développement de la civilisation.`,
+      year: "Year 3",
+    },
+  },
+  {
+    imgSrc: "artworks/4.jpg",
+    width: 5,
+    height: 3,
+    position: { x: 15, y: 2, z: -19.5 },
+    rotationY: 0,
+    info: {
+      title: "Révolution de l'Écriture",
+      artist: "Vincent van Gogh",
+      description: `L'invention de l'écriture a révolutionné la communication en permettant la transmission d'informations sur des supports durables tels que le papier, la pierre et le papyrus.`,
+      year: "Year 4",
+    },
+  }, // Back Wall
+
+  {
+    imgSrc: "artworks/5.jpg",
+    width: 5,
+    height: 3,
+    position: { x: -15, y: 2, z: 19.5 },
     rotationY: Math.PI,
     info: {
-      title: `Van Gogh ${i + 5}`,
-      artist: 'Vincent van Gogh',
-      description: `Artwork ${
-        i + 5
-      } by Vincent van Gogh is an exceptional piece showcasing his remarkable ability to capture emotion and atmosphere.`,
-      year: `Year ${i + 5}`,
+      title: "Messagerie et Courrier",
+      artist: "Vincent van Gogh",
+      description: `Les systèmes de messagerie et de courrier, tels que les pigeons voyageurs et les services postaux, ont permis des échanges rapides sur de longues distances, facilitant le commerce et la diplomatie.`,
+      year: "Year 5",
     },
-  })),
-  // Left Wall
-  ...Array.from({ length: 4 }, (_, i) => ({
-    imgSrc: `artworks/${i + 9}.jpg`,
+  },
+  {
+    imgSrc: "artworks/6.jpg",
     width: 5,
     height: 3,
-    position: { x: -19.5, y: 2, z: -15 + 10 * i },
+    position: { x: -5, y: 2, z: 19.5 },
+    rotationY: Math.PI,
+    info: {
+      title: "Avènement des Télécommunications",
+      artist: "Vincent van Gogh",
+      description: `La révolution industrielle a vu l'avènement des télécommunications, avec des inventions telles que le télégraphe et le téléphone, qui ont rendu la communication à distance plus rapide et plus efficace.`,
+      year: "Year 6",
+    },
+  },
+  {
+    imgSrc: "artworks/7.jpg",
+    width: 5,
+    height: 3,
+    position: { x: 5, y: 2, z: 19.5 },
+    rotationY: Math.PI,
+    info: {
+      title: "Médias de Masse",
+      artist: "Vincent van Gogh",
+      description: `Le développement de la radio et de la télévision a introduit les médias de masse, permettant la diffusion d'informations et de divertissements à un public plus large que jamais.`,
+      year: "Year 7",
+    },
+  },
+  {
+    imgSrc: "artworks/8.jpg",
+    width: 5,
+    height: 3,
+    position: { x: 15, y: 2, z: 19.5 },
+    rotationY: Math.PI,
+    info: {
+      title: "Ère de l'Informatique et de l'Internet",
+      artist: "Vincent van Gogh",
+      description: `L'essor de l'informatique et de l'internet dans la seconde moitié du 20e siècle a transformé la communication en permettant des échanges instantanés et mondiaux de données et de messages.`,
+      year: "Year 8",
+    },
+  }, // Left Wall
+
+  {
+    imgSrc: "artworks/9.jpg",
+    width: 5,
+    height: 3,
+    position: { x: -19.5, y: 2, z: -15 },
     rotationY: Math.PI / 2,
     info: {
-      title: `Van Gogh ${i + 9}`,
-      artist: 'Vincent van Gogh',
-      description: `With its striking use of color and brushwork, Artwork ${
-        i + 9
-      } is a testament to Van Gogh's artistic genius.`,
-      year: `Year ${i + 9}`,
+      title: "Réseaux Sociaux",
+      artist: "Vincent van Gogh",
+      description: `Les réseaux sociaux ont révolutionné la manière dont les individus interagissent en ligne, offrant des plateformes pour le partage de contenu, la communication en temps réel et la création de communautés virtuelles.`,
+      year: "Year 9",
     },
-  })),
-  // Right Wall
-  ...Array.from({ length: 4 }, (_, i) => ({
-    imgSrc: `artworks/${i + 13}.jpg`,
+  },
+  {
+    imgSrc: "artworks/10.jpg",
     width: 5,
     height: 3,
-    position: { x: 19.5, y: 2, z: -15 + 10 * i },
+    position: { x: -19.5, y: 2, z: -5 },
+    rotationY: Math.PI / 2,
+    info: {
+      title: "Accessibilité de la Communication",
+      artist: "Vincent van Gogh",
+      description: `Les smartphones ont rendu la communication accessible à tout moment et en tout lieu, avec des fonctionnalités telles que les appels, les messages texte, et l'accès à internet.`,
+      year: "Year 10",
+    },
+  },
+  {
+    imgSrc: "artworks/11.jpg",
+    width: 5,
+    height: 3,
+    position: { x: -19.5, y: 2, z: 5 },
+    rotationY: Math.PI / 2,
+    info: {
+      title: "Messagerie Instantanée et Réseaux Sociaux",
+      artist: "Vincent van Gogh",
+      description: `Les applications de messagerie instantanée et les réseaux sociaux ont transformé la communication personnelle et professionnelle en permettant des conversations multimédias et des interactions de groupe.`,
+      year: "Year 11",
+    },
+  },
+  {
+    imgSrc: "artworks/12.jpg",
+    width: 5,
+    height: 3,
+    position: { x: -19.5, y: 2, z: 15 },
+    rotationY: Math.PI / 2,
+    info: {
+      title: "Utilisation de la Vidéoconférence",
+      artist: "Vincent van Gogh",
+      description: `La vidéoconférence est devenue un outil essentiel pour les réunions d'affaires, l'éducation à distance et les interactions sociales, offrant un moyen pratique de se connecter en temps réel.`,
+      year: "Year 12",
+    },
+  }, // Right Wall
+
+  {
+    imgSrc: "artworks/13.jpg",
+    width: 5,
+    height: 3,
+    position: { x: 19.5, y: 2, z: -15 },
     rotationY: -Math.PI / 2,
     info: {
-      title: `Van Gogh ${i + 13}`,
-      artist: 'Vincent van Gogh',
-      description: `Artwork ${
-        i + 13
-      } is a captivating piece by Vincent van Gogh, reflecting his distinctive style and deep passion for art.`,
-      year: `Year ${i + 13}`,
+      title: "Élargissement du Paysage Médiatique",
+      artist: "Vincent van Gogh",
+      description: `Les podcasts et les chaînes YouTube ont élargi le paysage médiatique en offrant des contenus audiovisuels sur une multitude de sujets, accessibles à tout moment.`,
+      year: "Year 13",
     },
-  })),
+  },
+  {
+    imgSrc: "artworks/14.jpg",
+    width: 5,
+    height: 3,
+    position: { x: 19.5, y: 2, z: -5 },
+    rotationY: -Math.PI / 2,
+    info: {
+      title: "Transformation de la Consommation des Médias",
+      artist: "Vincent van Gogh",
+      description: `Les plateformes de streaming ont transformé la manière dont les gens consomment les médias en offrant un accès à une vaste bibliothèque de films, de séries et de musique.`,
+      year: "Year 14",
+    },
+  },
+  {
+    imgSrc: "artworks/15.jpg",
+    width: 5,
+    height: 3,
+    position: { x: 19.5, y: 2, z: 5 },
+    rotationY: -Math.PI / 2,
+    info: {
+      title: "Expériences Immersives avec la Réalité Virtuelle et Augmentée",
+      artist: "Vincent van Gogh",
+      description: `La réalité virtuelle et augmentée ouvre de nouvelles possibilités pour la communication en offrant des expériences immersives et interactives, tant dans le divertissement que dans l'éducation.`,
+      year: "Year 15",
+    },
+  },
+  {
+    imgSrc: "artworks/16.jpg",
+    width: 5,
+    height: 3,
+    position: { x: 19.5, y: 2, z: 15 },
+    rotationY: -Math.PI / 2,
+    info: {
+      title: "Adaptation de la Communication à l'Évolution Technologique",
+      artist: "Vincent van Gogh",
+      description: `Alors que la technologie continue d'évoluer, la communication humaine s'adapte en permanence pour répondre aux besoins changeants des individus et des sociétés, tout en préservant les fondements de la connexion humaine.`,
+      year: "Year 16",
+    },
+  },
 ];
